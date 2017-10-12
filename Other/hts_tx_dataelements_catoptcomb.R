bd313bdd92cebd4e6c5f52e682ac5dd3da69a510
Aaron <achafetz@usaid.gov>
2017-10-12 10:40:39 -0400

    results only

--- Other/hts_tx_dataelements_catoptcomb.R
+++ Other/hts_tx_dataelements_catoptcomb.R
@@ -10,12 +10,17 @@
 #import Fact View Q3v2.1 dataset
   df_mer  <- read_tsv("Documents/ICPI/Data/ICPI_FactView_OU_IM_20170922_v2_1.txt")
     names(df_mer) <- tolower(names(df_mer)) #convert names to lower case
-
-#identify all data element uids and category option combos possible
+    
+    df_mer[is.na(df_mer)] <- 0
+    
+#identify all results data element uids and category option combos possible
   mcad <- df_mer %>% 
     filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "TX_CURR")) %>%
-    distinct(indicator, indicatortype, standardizeddisaggregate, dataelementuid, ismcad, categoryoptioncomboname, categoryoptioncombouid, age, sex, resultstatus, modality, otherdisaggregate) %>% 
-    select(indicator, indicatortype, standardizeddisaggregate, dataelementuid,ismcad, categoryoptioncomboname, categoryoptioncombouid, age, sex, resultstatus, modality, otherdisaggregate) %>%
+    mutate(fy2017cum = ifelse(indicator=="TX_CURR", fy2017q3, fy2017q1 + fy2017q2 + fy2017q3)) %>% 
+    group_by(indicator, indicatortype, standardizeddisaggregate, dataelementuid, ismcad, categoryoptioncomboname, categoryoptioncombouid, age, sex, resultstatus, modality, otherdisaggregate) %>%
+    summarize_at(vars(fy2017cum), funs(sum(., na.rm=TRUE))) %>%
+    select(indicator, indicatortype, standardizeddisaggregate, dataelementuid,ismcad, categoryoptioncomboname, categoryoptioncombouid, age, sex, resultstatus, modality, otherdisaggregate, fy2017cum)%>%
+    filter(fy2017cum != 0) %>%
 #export file  
     write_tsv("Downloads/hts_tx_uids_catoptcombo.txt")
 
