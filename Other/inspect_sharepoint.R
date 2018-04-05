7f6e72fcd34b7f8b10b93cc47719d1243315cd2a
achafetz <achafetz@usaid.gov>
2018-04-05 11:48:02 -0400

    add visuals

--- Other/inspect_sharepoint.R
+++ Other/inspect_sharepoint.R
@@ -15,7 +15,7 @@
   library(tidyverse)
   library(readxl)
   library(lubridate)
-
+  library(scales)
 
 # Import and Munge --------------------------------------------------------
 
@@ -34,12 +34,13 @@
 
 #remove completely blank/meaningless columns
   sharepoint <- sharepoint %>% 
-    select(-site_id, -item_id, -custom_event_name, -event_data, -event_source, -source_name, -app_id) 
+    select(-site_id, -item_id, -custom_event_name, -event_data, -event_source, -source_name, -app_id)
 
 #split out name and id
   sharepoint <- sharepoint %>% 
       separate(user_id, c("user", "user_id"), sep = "<") %>% 
-      mutate(user_id = str_replace(user_id, "i:0#.w\\|pepfar\\\\", "")  %>% str_replace(">", ""))
+      mutate(user_id = str_replace(user_id, "i:0#.w\\|pepfar\\\\", "")  %>% str_replace(">", ""),
+             user = trimws(user))
 
 #keep only documents (remove List and Folder) and only care about views - distinct(sharepoint, event)
   sharepoint <- sharepoint %>% 
@@ -49,7 +50,7 @@
 #extract document name from document_location
   sharepoint <- sharepoint %>% 
       mutate(document = str_extract(document_location, "/[^/]*$") %>% str_replace("/", ""),
-             file_type = str_extract(document, "\\.[^\\.]*$") %>% str_replace("\\.", "") %>% tolower()
+             document_type = str_extract(document, "\\.[^\\.]*$") %>% str_replace("\\.", "") %>% tolower()
           ) %>% 
       select(-document_location) 
   
@@ -57,10 +58,13 @@
   sharepoint <- sharepoint %>% 
       mutate(datetime = ymd_hms(occurred_gmt, tz = "GMT") %>% with_tz("America/New_York")) %>% 
       select(-occurred_gmt) %>% 
-      mutate(month = month(datetime, label = TRUE),
-             day = wday(datetime, label = TRUE)
+      mutate(year = year(datetime),
+             month = month(datetime, label = TRUE),
+             day = day(datetime),
+             wday = wday(datetime, label = TRUE),
+             date = ymd(paste(year, month, day, sep = "-"))
       ) 
-    
+
 # Inspect -----------------------------------------------------------------
   
 #how many distinct users were there?
@@ -69,13 +73,41 @@
 #how many document were there?
   n_distinct(sharepoint$document) 
   
+#what are the different file types
+  sharepoint %>% 
+    distinct(document_type) %>% 
+    arrange(document_type)
+  
+#how many distinct documents by document type?
+  sharepoint %>% 
+    distinct(document,document_type) %>% 
+    arrange(document_type) %>% 
+    count(document_type, sort = TRUE)
+  
+#how many potential tools (xl*)
+  sharepoint %>% 
+    distinct(document, document_type) %>% 
+    filter(str_detect(document_type, "xl|zip")) %>% 
+    count(document_type, sort = TRUE)
+
+#how many Views by file type
+  sharepoint %>% 
+    distinct(user, document, document_type) %>% 
+    count(document_type, sort = TRUE)
+  
+#how many Views of potential tools (xl*)
+  sharepoint %>% 
+    distinct(user, document, document_type) %>% 
+    filter(str_detect(document_type, "xl|zip")) %>% 
+    count(document_type, sort = TRUE)
+  
 #how many total downloads per user (max 1 per tool)
   sharepoint %>% 
     distinct(user, document) %>% 
     count(user) %>% 
     arrange(desc(n))
      
-#how many total downloads per user (max 1 per tool)
+#how many total downloads per document (max 1 per tool)
    sharepoint %>% 
      distinct(user, document) %>% 
      count(document) %>% 
@@ -87,7 +119,70 @@
        count(month)    
 
 
+
+# Visualize ---------------------------------------------------------------
+
+#daily traffic
+   sharepoint %>% 
+     filter(month != "Sep") %>%
+     distinct(user, document, date) %>% 
+     count(date) %>% 
+     ggplot(aes(date, n)) +
+     geom_col() +
+     labs(title = "ICPI Sharepoint Traffic", 
+          subtitle = "Views per day during Q1", 
+          x = "", y = "", 
+          caption = "Views encompass both uploads or downloads")
+  
+ #leaderboard 
+   sharepoint %>% 
+     distinct(user, document) %>% 
+     count(user) %>% 
+     filter(n > quantile(n, .9)) %>%  
+     ggplot(aes(reorder(user, n), n)) +
+       geom_col() +
+       coord_flip() +
+       labs(title = "ICPI FY18Q1 Leaderboard", 
+          subtitle = "Users in the top 90% of Total Views from the ICPI site", 
+          x = "Views", y = "", 
+          caption = "Views encompass both uploads or downloads") 
    
-#any way to distinguish between downloads and uploads? 
-#why so many views within seconds of the same document?
-     
+  #downloads per day
+   
+  sharepoint %>% 
+    distinct(user, document, wday) %>% 
+    count(wday) %>% 
+    ggplot(aes(wday, n)) +
+    geom_col() +
+    scale_y_continuous(labels = comma) +
+    labs(title = "Hot Days", 
+         subtitle = "ICPI Views by weekday in FY18Q1", 
+         x = "Views", y = "", 
+         caption = "Views encompass both uploads or downloads")
+  
+#docs by type
+  sharepoint %>% 
+    distinct(document, document_type) %>% 
+    count(document_type) %>% 
+    filter(!is.na(document_type)) %>% 
+    ggplot(aes(reorder(document_type, n), n)) + 
+      geom_col() +
+      coord_flip() +
+      scale_y_continuous(labels = comma) +
+      labs(title = "File Types", 
+           subtitle = "File extensions on ICPI's Sharepoint", 
+           x = "FIles", y = "")
+#views by docs by type
+  sharepoint %>% 
+    distinct(user, document, document_type) %>% 
+    count(document_type) %>% 
+    filter(!is.na(document_type)) %>% 
+    ggplot(aes(reorder(document_type, n), n)) + 
+    geom_col() +
+    coord_flip() +
+    scale_y_continuous(labels = comma) +
+    labs(title = "File Types Views", 
+         subtitle = "File extensions views on ICPI's Sharepoint", 
+         x = "FIles", y = "",
+         caption = "Views encompass both uploads or downloads")
+  
